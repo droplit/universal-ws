a92471791c6315673978e001b4a70c384ecf9de6
Sabbir A. Baser <sabbir@droplit.io>
2018-06-05 19:46:50 -0400

    Add Access functions Add Acknowledgements to Session

--- src/server/access.ts
+++ src/server/access.ts
@@ -1,20 +1,51 @@
+import { EventEmitter } from 'events';
 import { Session, WsContext } from './session';
 import * as http from 'http';
 
-export class Access {
+export class Access extends EventEmitter {
 
     private session: Session;
 
-    constructor(server: http.Server, authenticator?: (connection: WsContext) => boolean) {
+    constructor(server: http.Server, authenticator?: (connection: WsContext) => Promise<boolean>) {
+        super();
         this.session = new Session(server, authenticator);
+        this.session.on('connection', this.connection);
+        this.session.on('connected', this.connected);
+        this.session.on('disconnected', this.disconnected);
+    }
 
+    // A new connection that is yet to be authenticated or established
+    public connection(connection: WsContext) {
+        this.emit('connection', connection);
+    }
+
+    // A new authenticated/established connection
+    public connected(connection: WsContext) {
+        this.emit('connected', connection);
+    }
+
+    // A connection dropped or closed
+    public disconnected(connection: WsContext) {
+        this.emit('disconnected', connection);
     }
 
     public onMessage(message: string, handler: (clientId: string, data: any, context: any) => void) {
-        this.session.on(`${message}`, (clientId, data, context) => {
+        this.session.on('message', (message, clientId, data, context) => {
             handler(clientId, data, context);
         });
     }
 
-    // public onRequest(message: string, handler: (clientId: string, data: any, context: any, )) HOW DIFFERENTIATE? IS IT NECESSARY?
+    public onRequest(message: string, handler: (clientId: string, data: any, context: any, callback: (result: any, expectAcknowledgement: boolean, timeout: number) => Promise<any>) => void) {
+        this.session.on('request', (message, clientId, data, context, callback) => {
+            handler(clientId, data, context, callback);
+        });
+    }
+
+    public requestAuthentication(connection: WsContext) {
+        this.session.requestAuthentication(connection);
+    }
+
+    // public send(message: string) {
+    //     // this.session.
+    // }
 }
--- src/server/session.ts
+++ src/server/session.ts
@@ -3,43 +3,60 @@ import * as http from 'http';
 import { Transport } from './transport';
 import * as WebSocket from 'ws';
 
+const ObjectId = require('bson-objectid');
+
 export interface WsContext<Context = any> extends WebSocket {
     context: Context;
     lastHeartbeat: Date;
     waiting: (() => void)[];
+    acknowledgements: {
+        [ackId: string]: {
+            timer: any;
+            callback: (response: any, error: any) => Promise<any>;
+        }
+    };
 }
 
 export interface StandardPacket {
-    t?: 'hb' | 'hbr';
+    t?: 'hb' | 'hbr' | string;
     m: string;
     d: any;
-    r?: boolean;
+    r?: boolean | string;
     i: string;
 }
 
 export class Session extends EventEmitter {
 
     private transport: Transport;
-    public connections: WebSocket[] = [];
+    private authenticator?: (connection: WsContext) => Promise<boolean>;
+    public connections: WsContext[] = [];
 
-    constructor(server: http.Server, authenticator?: (connection: WsContext) => boolean) {
+    constructor(server: http.Server, authenticator?: (connection: WsContext) => Promise<boolean>) {
         super();
         this.transport = new Transport(server);
 
         this.transport.on('connection', (connection: WsContext) => {
+            this.emit('connection', connection);
             if (authenticator) {
-                if (authenticator(connection)) {
-                    this.onConnectionActive(connection);
-                    this.connectionReady(connection);
-                    this.renewHeartbeat(connection);
-                    this.connections.push(connection);
-                } else {
-                    connection.close(1008, JSON.stringify({ code: 1008, reason: 'auth' }));
-                }
+                this.authenticator = authenticator;
+                authenticator(connection)
+                    .then((result) => {
+
+                        this.onConnectionActive(connection);
+                        this.connectionReady(connection);
+                        this.connections.push(connection);
+                        this.emit('connected', connection);
+                    })
+                    .catch((error) => {
+                        this.emit('disconnected', connection);
+                        connection.close(1008, JSON.stringify({ code: 1008, reason: 'auth' }));
+                    });
             } else {
+                this.authenticator = (connection) => Promise.resolve(true);
                 // No need to authenticate
                 this.renewHeartbeat(connection);
                 this.connections.push(connection);
+                this.emit('connected', connection);
             }
 
             connection.on('message', (data) => {
@@ -107,26 +124,68 @@ export class Session extends EventEmitter {
                 this.renewHeartbeat(connection);
             } else if (packet.t === 'hbr') {
                 // Send connection a heartbeat response?
+
                 this.renewHeartbeat(connection);
             }
 
             if (packet.r === undefined) {
                 // Handle message
-                this.emit(`${packet.m}`, packet.i, packet.d, connection.context, (result: any) => {
+                this.emit('message', packet.m, packet.i, packet.d, connection.context, (result: any) => {
                     // Done
                 });
             } else if (packet.r === true) {
                 // Handle request expecting a response
-                this.emit(`${packet.m}`, packet.i, packet.d, connection.context, (result: any) => {
-                    const response = {
+                this.emit('request', packet.m, packet.i, packet.d, connection.context, (result: any, expectAcknowledgement: boolean = false, timeout: number = 5000) => {
+                    const response: Partial<StandardPacket> = {
+                        m: JSON.stringify(packet.m),
                         d: result,
                         r: JSON.stringify(packet.i)
                     };
+                    if (expectAcknowledgement) {
+                        const ackId: string = ObjectId();
+                        response.t = ackId;
+                        connection.acknowledgements[ackId] = {
+                            callback: (response: any, error: any) => {
+                                if (error) {
+                                    return Promise.reject(error);
+                                } else {
+                                    return Promise.resolve(response);
+                                }
+                            },
+                            timer: setTimeout(() => {
+                                // Timed out in acknowledging response
+                                clearTimeout(connection.acknowledgements[ackId].timer);
+                                connection.acknowledgements[ackId].callback(undefined, 'Acknowledgement timed out');
+                                delete connection.acknowledgements[ackId];
+                            }, timeout)
+                        };
+                    } else {
+                        return Promise.resolve();
+                    }
 
                     connection.send(JSON.stringify(response));
                 });
             }
         });
+    }
+
+    public requestAuthentication(connection: WsContext) {
+        if (this.authenticator) {
+            this.authenticator(connection)
+                .then((result) => {
+                    if (result) {
+                        this.onConnectionActive(connection);
+                        this.connectionReady(connection);
+                        this.emit('authenticated', connection);
+                    }
+                })
+                .catch((error) => {
+                    this.emit('disconnected', connection);
+                });
+        } else {
+            this.emit('authenticated', connection);
+
+        }
     }
 
 }
