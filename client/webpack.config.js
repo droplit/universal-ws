6166063d4b711617e4a447398b8f7d2c7f72eaa6
Chris Woodle <chris@droplit.io>
2018-11-12 21:49:50 -0500

    Added browser build version

---
+++ client/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+var webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    entry: './dist/library.js',
+    output: {
+        path: path.resolve(__dirname, 'browser'),
+        filename: 'universal-ws.js',
+        library: 'universal-ws',
+        libraryTarget: 'umd'
+    },
+    externals: [
+        'ws'
+    ],
+    plugins: [
+        new webpack.IgnorePlugin(/ws(?!er)/)
+    ]
+};
