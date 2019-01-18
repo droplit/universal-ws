5b436d3b7b06c5ff1827315e78ef8ad04b0b9e6b
Sabbir A. Baser <sabbir@droplit.io>
2019-01-18 00:19:34 -0500

    Add client and server tests

---
+++ test/system/client.test.ts
@@ -0,0 +1,26 @@
+/// <reference types="mocha" />
+import { expect } from 'chai';
+import { UniversalWebSocket } from '../../client/dist/library';
+
+const PORT = 3005;
+const AUTHENTICATED_PORT = 3006;
+
+const clients: UniversalWebSocket[] = [];
+
+describe('WebSockets', function () {
+    this.timeout(10000);
+
+    it(`Initialize a client by connecting to port ${PORT}`, function (done) {
+        const client = new UniversalWebSocket(`ws://localhost:${PORT}`);
+        expect(client).to.exist;
+        clients.push(client);
+        done();
+    });
+
+    it(`Initialize an authenticated client by connecting to port ${AUTHENTICATED_PORT}`, function (done) {
+        const client = new UniversalWebSocket(`ws://localhost:${AUTHENTICATED_PORT}`, { username: 'boats', password: 'USS History Supreme' });
+        expect(client).to.exist;
+        clients.push(client);
+        done();
+    });
+});
---
+++ test/system/server.test.ts
@@ -0,0 +1,66 @@
+/// <reference types="mocha" />
+
+import { expect } from 'chai';
+import { createServer, Server } from 'http';
+// import * as express from 'express';
+import { UniversalWebSocketServer, Client, } from '../../server/dist/library';
+
+const PORT = 3005;
+const AUTHENTICATED_PORT = 3006;
+
+describe('Universal WS Server', function () {
+    this.timeout(10 * 1000);
+
+    let httpServer: Server;
+    let uws: UniversalWebSocketServer;
+    const clients: Client[] = [];
+    let authenticatedHttpServer: Server;
+    let authenticatedUws: UniversalWebSocketServer;
+    const authenticatedClients: Client[] = [];
+
+    it(`Initialize the server with port ${PORT}`, function (done) {
+        httpServer = createServer();
+        expect(httpServer).to.exist;
+        httpServer.listen(PORT, () => {
+            console.log('Server listening to port:', PORT);
+        });
+        uws = new UniversalWebSocketServer(httpServer);
+        expect(uws).to.exist;
+
+        done();
+    });
+
+    it(`Handle a new client connecting`, function (done) {
+        uws.on('connected', (client: Client) => {
+            expect(client).to.exist;
+            clients.push(client);
+            done();
+        });
+    });
+
+    it(`Initialize the authenticated server with port ${AUTHENTICATED_PORT}`, function (done) {
+        authenticatedHttpServer = createServer();
+        expect(authenticatedHttpServer).to.exist;
+        authenticatedHttpServer.listen(AUTHENTICATED_PORT, () => {
+            console.log('Authenticated Server listening to port:', AUTHENTICATED_PORT);
+        });
+        authenticatedUws = new UniversalWebSocketServer(httpServer);
+        expect(authenticatedUws).to.exist;
+
+        done();
+    });
+
+    it(`Handle a new authenticated client connecting`, function (done) {
+        authenticatedUws.on('connected', (client: Client) => {
+            expect(client).to.exist;
+            expect(client.username).to.exist;
+            console.log(`Client ${client.username} connected. Their password is ${client.password}`);
+            expect(client.username).to.equal('boats');
+            expect(client.password).to.equal('USS History Supreme');
+
+            authenticatedClients.push(client);
+            done();
+        });
+    });
+
+});
